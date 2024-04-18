9cc8cf48d58e18afd19dc43c43b4510699f5aa1a
BreadOfPipas <sbareacela@gmail.com>
2024-04-19 01:41:17 +0200

    Commit build 0.5

--- src/constants.js
+++ src/constants.js
@@ -7,8 +7,8 @@ export const dialogueData = {
   como ya he dicho otras veces y se que parece solo que soy un pelota, pero de verdad que me alegras los dias y me llenas los dias de color solo con verte sonreir o en si verte.
   Eso es algo que nada ni nadie mas consigue hacer, te lo aseguro. ... -> lee la carta de la derecha`,
   carta2: ``,
-  cama: `Esta es simplemente mi cama, donde suelo dormir. *Se puede ver como esta la cama medio abierta con desdentao aplastujao del abrazo del oso que le ha dado durmiendo* <img src="img/cama.png" height="10%" width="10%" style="display: flex">`,
-  corazon: `<img src="img/howl_sophie.png" width="20%" height="20%" style="display: flex"> <em>"Un corazon es una carga pesada"</em><br>El no tenia corazon y ella le devolvio la calidez a su alma.`,
-  estanteria: `Se puede observar una gran coleccion de mangas y figuritas. Entre todas destaca un Yoh con dos cuerditas rojas en la katana. Se oye un echo de fondo a lo lejos ... <em>Casa-dittos.... Casa-dittos...</em> <img src="img/ditto.png" height="30%" width="30%" style="display: flex">`,
+  cama: `Esta es simplemente mi cama, donde suelo dormir. *Se puede ver como esta la cama medio abierta con desdentao aplastujao del abrazo del oso que le ha dado durmiendo* <img src="./cama.png" height="10%" width="10%" style="display: flex">`,
+  corazon: `<img src="./howl_sophie.png" width="20%" height="20%" style="display: flex"> <em>"Un corazon es una carga pesada"</em><br>El no tenia corazon y ella le devolvio la calidez a su alma.`,
+  estanteria: `Se puede observar una gran coleccion de mangas y figuritas. Entre todas destaca un Yoh con dos cuerditas rojas en la katana. Se oye un echo de fondo a lo lejos ... <em>Casa-dittos.... Casa-dittos...</em> <img src="./ditto.png" height="30%" width="30%" style="display: flex">`,
   foto: `Esta es la mesita de noche donde veo nuestra foto, siempre le doy un beso antes de dormir 👉🏻👈🏻`
 };
