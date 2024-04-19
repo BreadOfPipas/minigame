eaf1b9065f6bceac1f78f28177d9a2ff96d25e81
BreadOfPipas <sbareacela@gmail.com>
2024-04-19 17:03:50 +0200

    Build 0.8

--- src/constants.js
+++ src/constants.js
@@ -6,9 +6,16 @@ export const dialogueData = {
   nada mas que quiera en el mundo que estar contigo. Estos años han sido los mejores de toda mi vida, sin exagerarte. Jamas me he sentido tan feliz como lo he sido a tu lado,
   como ya he dicho otras veces y se que parece solo que soy un pelota, pero de verdad que me alegras los dias y me llenas los dias de color solo con verte sonreir o en si verte.
   Eso es algo que nada ni nadie mas consigue hacer, te lo aseguro. ... -> lee la carta de la derecha`,
-  carta2: ``,
+  carta2: `Con todo lo dicho en la anterior carta y habiendolo razonado estos dias... el corazon me dice que te amo. He cambiado el "chip" por asi decirlo, o darme dos tortazos para despertarme que me gusta mas decirlo asi uwu,
+  y lo que quiero decir con eso es que voy a hacerte sonreir de nuevo, ver de nuevo la sonrisa en tu rostro a diario y ver como progresas. Te voy a demostrar como de verdad puedo cambiar y en parte ya he cambiado mentalmente (aunque me falte demostrarlo bien si).
+  Y para finalizar esta carta, queria decirte que a parte de las cosas que he soltado por aqui para expresartelo todo, puedes entrar de nuevo cuando quieras para que te recuerde que te quiero con toda mi alma 🤍🦋🐉. <br>
+  PD: Quizas haya algo nuevo por la habitacion... 👀`,
   cama: `Esta es simplemente mi cama, donde suelo dormir. *Se puede ver como esta la cama medio abierta con desdentao aplastujao del abrazo del oso que le ha dado durmiendo* <img src="./cama.png" height="10%" width="10%" style="display: flex">`,
   corazon: `<img src="./howl_sophie.png" width="20%" height="20%" style="display: flex"> <em>"Un corazon es una carga pesada"</em><br>El no tenia corazon y ella le devolvio la calidez a su alma.`,
   estanteria: `Se puede observar una gran coleccion de mangas y figuritas. Entre todas destaca un Yoh con dos cuerditas rojas en la katana. Se oye un echo de fondo a lo lejos ... <em>Casa-dittos.... Casa-dittos...</em> <img src="./ditto.png" height="30%" width="30%" style="display: flex">`,
-  foto: `Esta es la mesita de noche donde veo nuestra foto, siempre le doy un beso antes de dormir 👉🏻👈🏻`
+  foto: `Esta es la mesita de noche donde veo nuestra foto, siempre le doy un beso antes de dormir 👉🏻👈🏻`,
+  tv: `El televisor esta encendido. Hay una pelicula puesta, parece ser que trata sobre algo asi como un ogro, un asno y una princesa... hmm parece que incluso hay 3 ratas ciegas... curioso 🤔
+  <br><img src="./shrek.jpg" width="20%", height="20%" style="display: flex">`,
+  pc: `Se ve a dos personas haciendo una videollamada... no se entiende bien que estan haciendo pero bueno, se lo estan pasando bien 🤭💖
+  <br><img src="./espelusnaos1.jpg" width="10%", height="10%"><img src="./espelusnaos2.jpg" width="10%", height="10%"> `,
 };
